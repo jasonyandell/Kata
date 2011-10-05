95ed24bdbf4ebba7254a1b4e20baecf92baf7b07
Jason Yandell <jasonyandell@gmail.com>
2011-10-04 20:43:59 -0500

    Added more failing tests

--- BowlingScore/BowlingScore/BowlingTests.cs
+++ BowlingScore/BowlingScore/BowlingTests.cs
@@ -9,20 +9,31 @@ namespace BowlingScore
     [TestClass]
     public class BowlingTests
     {
+        private BowlingScorer scorer;
 
-        [TestMethod]
-        public void can_create_the_class()
+        [TestInitialize]
+        public void TestInitialize()
         {
-            var scorer = new BowlingScorer();
+            scorer = new BowlingScorer();
         }
 
         [TestMethod]
         public void can_score_strike()
         {
-            var scorer = new BowlingScorer();
             Assert.AreEqual(10, scorer.Score("X"));
         }
 
+        [TestMethod]
+        public void can_score_spare()
+        {
+            Assert.AreEqual(10, scorer.Score("5/"));
+        }
+
+        [TestMethod]
+        public void can_score_mundane_frame()
+        {
+            Assert.AreEqual(7, scorer.Score("25"));
+        }
 
     }
 }
