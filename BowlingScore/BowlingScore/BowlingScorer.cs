c007b811c97a3f7b519a5b08aac2945b50465f77
Courtney Shrock <cshrock@diymediaservices.com>
2011-10-04 18:26:49 -0700

    add interfaces

---
+++ BowlingScore/BowlingScore/BowlingScorer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BowlingScore
+{
+    public class BowlingScorer : IBowlingScorer
+    {
+        protected int[] scores = new int[10];
+
+        public int Score(string input)
+        {
+            int score = 0;
+            for(int i=0; i<input.Length; i++) {
+                char t = input[i];
+                //if (t == 'X')
+            }
+            return scores.Sum();
+        }
+
+        public int ScoreFrame()
+        {
+            return 0;
+        }
+    }
+}
