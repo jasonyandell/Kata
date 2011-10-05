32e3effc6898c01e251747cc957a0b67c1d0c5e4
Courtney Shrock <cshrock@diymediaservices.com>
2011-10-04 19:41:13 -0700

    imp

--- BowlingScore/BowlingScore/BowlingScorer.cs
+++ BowlingScore/BowlingScore/BowlingScorer.cs
@@ -7,21 +7,69 @@ namespace BowlingScore
 {
     public class BowlingScorer : IBowlingScorer
     {
+        /*
+        protected string input;
         protected int[] scores = new int[10];
+
+        protected IEnumerable<char[]> FrameScores
+        {
+            get {
+                int s = 0;
+                char[] frame = new char[3];
+                for (int f = 0; f < input.Length; f++) {
+                    frame[s] = input[f];
+                    if (frame[s]
+                }
+            }
+        }
 
         public int Score(string input)
         {
-            int score = 0;
-            for(int i=0; i<input.Length; i++) {
-                char t = input[i];
-                //if (t == 'X')
-            }
-            return scores.Sum();
+            char t = input[0];
+
+            if (char.ToUpper(t) != 'X') 
+
+            string frame = string.Format("{0}", t, );
         }
 
         public int ScoreFrame()
         {
             return 0;
+        }
+         */
+
+        public int Score(string input)
+        {
+            List<string> frames = new List<string>();
+            string frame = string.Empty;
+
+            for (int f = 0; f < input.Length; f++) {
+                char t = input[f];
+                frame += t;
+                if (char.ToUpper(t) == 'X' || char.ToUpper(t) == '/') {
+                    frames.Add(frame);
+                    frame = string.Empty;
+                }
+            }
+            if (frame != string.Empty)
+                frames.Add(frame);
+
+            return frames.Select(f => ScoreFrame(f)).Sum();
+        }
+
+        public static int ScoreFrame(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return 0;
+            char t = input[0];
+            if (char.ToUpper(t) == 'X') {
+                return 10;
+            } else if (char.ToUpper(t) == '/') {
+                return 10;
+            } else {
+                int score = (int)Char.GetNumericValue(t);
+                score += ScoreFrame(input.Substring(1));
+                return score;
+            }
         }
     }
 }
