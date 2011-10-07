680d02b64dd9727549daceb7530df4978187e079
Jason Yandell <jasonyandell@gmail.com>
2011-10-07 09:42:57 -0500

    Solved Problem 15 from Project Euler

---
+++ JYAPES/ProblemFifteen/ProblemFifteen.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.net
+
+module ProblemFifteen
+
+let mutable Solutions = new System.Collections.Generic.Dictionary<int, list<BigNum>>()
+
+let rec PascalsTriangle:(int -> list<BigNum>) = function
+  | 1 -> [1N] 
+  | 2 -> [1N;1N]
+  | n when Solutions.ContainsKey(n) -> 
+    Solutions.[n]
+  | n when n>2 -> 
+      Solutions.Add(n,       
+        seq {       
+          yield 1N;
+          for i in 2..n-1 ->
+            let tri = PascalsTriangle (n-1)
+            (List.nth tri (i-1)) + (List.nth tri (i-2))
+          yield 1N
+        } |> Seq.toList)
+      Solutions.[n]
+  | x -> failwith (x.ToString())
+
+let PathCount n =
+  let depth = ((n*2)+1)
+  PascalsTriangle depth |> Seq.nth ((depth-1)/2)
+
---
+++ JYAPES/ProblemFifteen/Tests.fs
@@ -0,0 +1,45 @@
+﻿namespace Tests
+
+open NUnit.Framework
+open FsUnit
+
+[<TestFixture>]
+type ``Pascal's Triangle`` ()=
+  let f = ProblemFifteen.PascalsTriangle
+
+  [<Test>]member test.
+   ``1 -> [1]`` ()=
+     f 1 |> should equal [1N]
+
+  [<Test>]member test.
+   ``2 -> [1;1]`` ()=
+     f 2 |> should equal [1N;1N]
+
+  [<Test>]member test.
+   ``3 -> [1;2;1]`` ()=
+     f 3 |> should equal [1N;2N;1N]
+
+  [<Test>]member test.
+   ``5 -> [1;4;6;4;1]`` ()=
+     f 5 |> should equal [1N;4N;6N;4N;1N]
+
+
+[<TestFixture>]
+type ``Path Counts`` ()=
+  [<Test>]member test.
+   ``0 -> 1`` ()=
+     ProblemFifteen.PathCount 0 |> should equal 1N
+
+  [<Test>]member test.
+   ``1 -> 2`` ()=
+     ProblemFifteen.PathCount 1 |> should equal 2N
+
+  [<Test>]member test.
+   ``2 -> 6`` ()=
+     ProblemFifteen.PathCount 2 |> should equal 6N
+
+  [<Test>]member test.
+   ``20  -> 137846528820`` ()=
+     ProblemFifteen.PathCount 20 |> should equal 137846528820N
+
+  
