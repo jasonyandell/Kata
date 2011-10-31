1dd6291fa27a5777dc4270b318e3cffcf2c22ef1
Jason Yandell <jasonyandell@gmail.com>
2011-10-30 23:32:34 -0500

    Got the answer submitted and credited.  Need to clean up unit tests and _remove mutable structures_

---
+++ JYAPES/ProblemTwentyThree/ProblemTwentyThree.fs
@@ -0,0 +1,57 @@
+﻿namespace ProblemTwentyThree
+
+open EulerCore
+open System.IO
+open System
+open System.Collections.Generic;
+
+type hash = System.Collections.Generic.HashSet<int>
+
+module Helpers = 
+
+    let abundantNumbers = 
+        let rec count i = 
+            seq {
+                if (NumberTheory.IsAbundant i) then yield i
+                yield! count (i+1)
+            }
+        count 1
+    
+    let abundantNumbersLessThan max = 
+        let abundants = new hash()
+        let sums = new hash()
+        let inRange n = n <= max
+
+        let updateSums n = 
+            abundants.Add(n) |> ignore
+            abundants 
+            |> Seq.iter (fun x -> 
+                sums.Add(x + n) |> ignore)
+
+        abundantNumbers 
+            |> Seq.takeWhile inRange
+            |> Seq.iter updateSums
+
+        sums
+
+//        let result = 
+//            sums 
+//            |> Seq.filter inRange
+//            |> Seq.toArray
+//
+//        result
+
+    let invertNumericSetUpToX max (set:hash) =
+        let h = new hash()
+        [1..max] |> List.iter (fun n -> h.Add(n) |> ignore) |> ignore
+        seq {
+            for item in set ->
+                h.Remove(item)
+        } |> Seq.toList |> ignore
+        h
+    
+    let Answer =
+        let set = invertNumericSetUpToX 28123 (abundantNumbersLessThan 28123)
+        Seq.fold (+) 0 set
+()
+
--- JYAPES/ProblemTwentyThree/Tests.fs
+++ JYAPES/ProblemTwentyThree/Tests.fs
@@ -1,50 +1,30 @@
-namespace ProblemTwentyOne.Tests
+namespace ProblemTwentyThree.Tests
 
 
+open ProblemTwentyThree
+open EulerCore
 open NUnit.Framework
 open FsUnit
 
 [<TestFixture>] 
-type ``Problem Twenty One: Amicable Pairs`` ()= 
-    let act n = (ProblemTwentyOne.ProperDivisors(n)).List
-    let d n = (ProblemTwentyOne.d.Of n)
-    let amicable (a,b) = (ProblemTwentyOne.CheckAmicable.For (a,b))
-    let allAmicableUnder n = (ProblemTwentyOne.CheckAmicable.ForNumbersLessThan n)
-    let a = allAmicableUnder
-    let y = 284 |> a |> Seq.toArray    
+type ``Problem Twenty Three: Abundant Numbers`` ()= 
 
     [<Test>] member test.
-     ``proper divisors of 2 is 1`` ()=
-        act 2 |> should equal [1]
+     ``Verify 12 is the smallest abundant number`` ()=
+        [1..12] |>
+            List.filter NumberTheory.IsAbundant
+            |> should equal [12]
 
     [<Test>] member test.
-     ``proper divisors of 4 is 1,2`` ()=
-        act 4 |> should equal [1;2]
+     ``Verify 24 is a sum of abundant numbers`` ()=
+        Helpers.abundantNumbersLessThan 24 |> should contain 24 
 
     [<Test>] member test.
-     ``proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110`` ()=
-        act 220 |> should equal [1; 2; 4; 5; 10; 11; 20; 22; 44; 55; 110]
+     ``Can compute list of all sums of two abundant numbers`` ()=        
+        Helpers.abundantNumbersLessThan 28123 |> should contain 6
 
     [<Test>] member test.
-     `` d(220) = 284`` ()=
-        d 220 |> should equal 284
+     ``Can find the answer`` ()=        
+        Helpers.Answer |> should equal 4179871
 
-    [<Test>] member test.
-     `` d(284) = 220`` ()=
-        d 284 |> should equal 220
-        
-    [<Test>] member test.
-     `` (220,284) is amicable`` ()=
-        (220, 284) |> amicable |> should equal true 
-
-    [<Test>] member test.
-     `` 220 is a member of allAmicableUnder 284`` ()=
-        y |> should contain 284
-
-    [<Test>] member test.
-     ``Answer to Question 21 is correct`` ()=
-        10000 
-            |> allAmicableUnder 
-            |> Seq.fold (+) 0
-            |> should equal 31626
 
